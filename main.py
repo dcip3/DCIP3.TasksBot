c60cc4f85ff7b4bc3984308d903373701655c1b1
dcip3 <keypoints.motion@gmail.com>
2025-11-14 14:42:05 +0100

    fix: enable uvicorn auto-reload only in dev mode

--- main.py
+++ main.py
@@ -105,6 +105,6 @@ if __name__ == "__main__":
         "main:app",
         host="0.0.0.0",
         port=8000,
-        reload=True,
+        reload=settings.dev_mode,
         log_level="info"
-    ) 
+    )
